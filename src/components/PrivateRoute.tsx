5e6d5588f606375cedfe85780b4f42d20085dc39
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-07-03 22:00:04 +0700

    edit

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,7 @@
 import { Navigate, To } from "react-router-dom";
-// import useAuth from "../components/UseAuth";
 import { useStoreSelector } from "../redux/hooks";
 
 function PrivateRoute({ children, to }: { children: JSX.Element; to: To }) {
-  // const { token } = useAuth();
   const { token } = useStoreSelector((state) => state.auth);
   if (!token) return <Navigate to={to} replace />;
   return children;
